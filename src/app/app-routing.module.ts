61f09329ee6389aae98159dc86cdaf510b8a4cbf
Jawhar <jawher.dhaou@esprit.tn>
2023-02-18 20:19:15 +0100

    navBar + Bootstrap ( working )

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
 
 
 const routes: Routes = [
-  {path:'', redirectTo:'products',pathMatch:'full'}
-  
+  {path:'', redirectTo:'products',pathMatch:'full'},
+  {path:'products', component:ProductComponent},
 ];
 
 @NgModule({
