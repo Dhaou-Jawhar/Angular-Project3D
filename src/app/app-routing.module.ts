9052c4cbad1ac552277b2a2f7d46780d0b6a653e
Jawhar <jawher.dhaou@esprit.tn>
2023-02-18 18:31:12 +0100

    Hot Fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 
+const routes: Routes = [
+  {path:'', redirectTo:'products',pathMatch:'full'}
+  
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
