c494ad7f84a5697998414f2ecff6aa44699e9aa7
Jawhar <jawher.dhaou@esprit.tn>
2023-02-23 11:03:22 +0100

    Service ( liste + add )

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Product } from '../core/model/product';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-add-product',
@@ -8,10 +10,19 @@ import { Product } from '../core/model/product';
 })
 export class AddProductComponent implements OnInit {
   product !: Product;
-  constructor() { }
+  constructor(private serviceProd:ProductService , private route:Router) { }
 
   ngOnInit(): void {
     this.product = new Product;
+  }
+
+  addProduct(){
+    //consume addProduct() from Service , and put product in parametre
+    this.serviceProd.addProduct(this.product);
+
+    //redirect after click add
+    this.route.navigateByUrl("/products");
+
   }
 
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../core/model/product';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -9,13 +10,13 @@ import { Product } from '../core/model/product';
 export class ProductComponent implements OnInit {
 title:string = "This The Product"
 listProduct!:Product[];
-  constructor() { }
+
+  //injecter le service dans le constructeur
+  constructor(private service: ProductService) { }
 
   ngOnInit(): void {
-    this.listProduct=[
-      {id: 1, title: "Hoodie", price: 18, quantity: 0, like: 0, object: "hoodie.glb"},
-      {id: 2, title: "Cap", price: 21, quantity: 10, like: 0, object: "Casquette.glb"},
-      {id: 3, title: "MUG", price: 16, quantity: 8, like: 0, object: "MUG.glb"}, ]
+    //affecter la liste de service dans la liste du produit
+    this.listProduct=this.service.listP
   }
 
   buy(p : number){
